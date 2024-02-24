00245885f38616ad61d07c9098adb3d06d973246
vadimka092 <150704842+vadimka092@users.noreply.github.com>
2024-02-24 11:47:10 +0300

    Update laba shishkin1.py

--- laba shishkin1.py
+++ laba shishkin1.py
@@ -1,37 +1,33 @@
-def number_to_words(num):
+numbers = []
+
+def is_even_and_hex(number):
+    if number % 2 == 0 and len(hex(number)[2:]) == int(str(number)[0]):
+        return True
+    return False
+
+with open('numbers.txt', 'r') as file:
+    numbers_str = file.readline().split()
+    numbers = [int(num) for num in numbers_str if num.isdigit()]
+
+
+max_number = max(numbers)
+
+def number_to_words(number):
     units = ["", "один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять"]
-    tens = ["", "десять", "двадцать", "тридцать", "сорок", "пятьдесят", "шестьдесят", "семьдесят", "восемьдесят",
-            "девяносто"]
-    hundreds = ["", "сто", "двести", "триста", "четыреста", "пятьсот", "шестьсот", "семьсот", "восемьсот", "девятьсот"]
-    if num == 0:
-        return "ноль"
-    if num < 0:
-        return "минус " + number_to_words(abs(num))
-    result = ""
-    if num >= 1000:
-        result += number_to_words(num // 1000) + " тысяч "
-        num %= 1000
-    if num >= 100:
-        result += hundreds[num // 100] + " "
-        num %= 100
-    if num >= 10:
-        if num >= 20:
-            result += tens[num // 10] + " "
-            num %= 10
-        else:
-            result += units[num] + " "
-            num = 0
-    if num > 0:
-        result += units[num]
-    return result
-count = 0
-for i in range(8, 204810):
-    hex_num = hex(i)[2:]
-    if len(hex_num) == str(hex_num)[0]:
-        decimal_num = int(hex_num, 16)
-        if decimal_num % 2 == 0:
-            count += 1
-            print(hex_num)
-max_num_in_words = number_to_words(int(hex_num, 16))
-print("Количество чисел:", count)
-print("Максимальное число прописью:", max_num_in_words)
+    teens = ["десять", "одиннадцать", "двенадцать", "тринадцать", "четырнадцать", "пятнадцать", "шестнадцать", "семнадцать", "восемнадцать", "девятнадцать"]
+    tens = ["", "", "двадцать", "тридцать", "сорок", "пятьдесят", "шестьдесят", "семьдесят", "восемьдесят", "девяносто"]
+    if number == 1000:
+        return "тысяча"
+    elif number >= 100:
+        return units[number // 100] + "сто " + number_to_words(number % 100)
+    elif number >= 20:
+        return tens[number // 10] + " " + units[number % 10]
+    elif number >= 10:
+        return teens[number - 10]
+    else:
+        return units[number]
+    print("Числа:")
+for number in numbers:
+    print(number, end=" ")
+    print("\nКоличество чисел:", len(numbers))
+    print("\nМаксимальное число: ", number_to_words(max_number))
